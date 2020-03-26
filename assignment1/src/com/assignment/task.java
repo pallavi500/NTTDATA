2839ec71e556ff3ffb3a3ef593457be5cea6823f
pallavi500 <pallavichiluveru@gmail.com>
2020-03-26 18:06:09 +0530

    Done

---
+++ assignment1/src/com/assignment/task.java
@@ -0,0 +1,14 @@
+package com.assignment;
+
+import java.util.Scanner;
+
+public class task {
+	public static void main(String[] args)
+	{
+		System.out.println("welcome pallavi");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Write something");
+		String p=sc.nextLine();
+	}
+
+}
